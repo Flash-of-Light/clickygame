db983a3fcb31204216c2d3cf0ede1a4ecdbe9060
Jay Goss <jayspgoss@gmail.com>
2019-01-23 23:44:05 -0500

    updates

---
+++ src/components/animate.js
@@ -0,0 +1,39 @@
+// import React, { Component } from "react";
+// import { render } from "react-dom";
+// import Hello from "./Hello";
+
+// const styles = {
+//   fontFamily: "sans-serif",
+//   textAlign: "center"
+// };
+
+// class App extends Component {
+//   state = {
+//     img: "https://i.vimeocdn.com/portrait/58832_300x300"
+//   };
+
+//   render() {
+//     return (
+//       <div style={styles}>
+//         <Hello name="CodeSandbox" />
+//         <h2>Start editing to see some magic happen {"\u2728"}</h2>
+//         <img
+//           src={this.state.img}
+//           onMouseEnter={() => {
+//             this.setState({
+//               img:
+//                 "http://www.toptipsclub.com/Images/page-img/keep-calm-and-prepare-for-a-test.png"
+//             });
+//           }}
+//           onMouseOut={() => {
+//             this.setState({
+//               img: "https://i.vimeocdn.com/portrait/58832_300x300"
+//             });
+//           }}
+//         />
+//       </div>
+//     );
+//   }
+// }
+
+// render(<App />, document.getElementById("root"));
